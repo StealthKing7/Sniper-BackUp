4847d3d818d496ccf94c7d6e1b87ddfae4a80e41
PredaKing2007 <91562271+PredaKing2007@users.noreply.github.com>
2022-03-06 22:09:44 +0300

    a

--- Sniper/Assets/Scripts/EnemyAI.cs
+++ Sniper/Assets/Scripts/EnemyAI.cs
@@ -5,12 +5,7 @@ using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
-    private NavMeshAgent Agent; 
-    // Start is called before the first frame update
-    void Start()
-    {
-        Agent = GetComponent<NavMeshAgent>();
-    }
+
 
     // Update is called once per frame
     void Update()
